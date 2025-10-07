44b8a8006e329c0e29c9c02be4db60d214a54f31
yukiiga <m241838@hiroshima-u.ac.jp>
2025-10-07 10:17:50 +0900

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// あなたのGitHubリポジトリ名（例: tenbin-game）をここに設定
-const repoName = 'tenbin';
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`,  // ← GitHub Pages用に追加
+  base: `/tenbin/`,  // ← GitHub Pages用に追加
   server: {
     port: 3000,
     open: true,
